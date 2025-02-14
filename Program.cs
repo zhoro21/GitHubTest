574909dcab9506b714faee774747320f9543d23e
Nikito69 <nikiniki2777@gmail.com>
2025-02-14 12:21:45 +0200

    add Print method

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
+            Print();
             Console.ReadKey();
+        }
+        
+        static void Print()
+        {
+            Console.WriteLine(Console.ReadLine());
         }
     }
 }
