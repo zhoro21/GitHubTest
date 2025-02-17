d330d61c91d8097caa868d37f084464b632921c2
MEGA <MEGA@gmail.com>
2025-02-17 12:19:21 +0200

    add print function

--- Program.cs
+++ Program.cs
@@ -2,16 +2,18 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int n = 1;
             Print();
             Console.ReadKey();
         }
         
-        static void Print()
+        static void Print(int n )
         {
-            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine($"{n++} {Console.ReadLine()}");
         }
     }
 }
