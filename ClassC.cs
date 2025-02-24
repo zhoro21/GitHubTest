55a91ae1244df41f9315bd10c2c40fb97d79f2eb
MEGA <MEGA@gmail.com>
2025-02-24 11:20:45 +0200

    Add classC

---
+++ ClassC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class ClassC
+{
+    public double bsqrt(double n)
+    {
+        return Math.Sqrt(n)
+    }
+    public double BabylonSqrt(double number)
+    {
+        dobule guess = number / 2;
+        while (Math.Abs(guess * guess - number) < 0.01)
+        {
+            double other = number / guess;
+            guess = (guess + other) / 2;
+        }
+        return guess;
+    }
+}
